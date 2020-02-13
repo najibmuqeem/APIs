3528cca9ebf1aa10c16e7382c5ebd405bc6a1324
Najib <najib.muqeem@gmail.com>
2020-02-13 18:49:41 +0000

    wrote tests for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,19 @@
 const request = require("request");
-const breed = process.argv[2];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  (error, response, body) => {
-    const data = JSON.parse(body);
-    if (error) {
-      console.log("Error", error);
-    } else if (data[0]) {
-      console.log(data[0].description);
-    } else {
-      console.log("Breed does not exist");
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      const data = JSON.parse(body);
+      if (error) {
+        callback(error, null);
+      } else if (data[0]) {
+        callback(null, data[0].description.trim());
+      } else {
+        callback(null, "Breed does not exist");
+      }
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
